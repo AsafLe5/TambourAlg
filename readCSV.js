85b8ff45374c9f442cad4b6b2ab7caac27205c6a
galsks <gal223344@gmail.com>
2021-06-03 20:16:18 +0300

    get dict

--- readCSV.js
+++ readCSV.js
@@ -1,5 +1,23 @@
-var fs = require('fs')
-fs.readFile('colors.csv', function (err, data) {
-    if (err) throw err;
-    console.log(data.toString());
-  });
+
+const Pixel = require('./Structures/Pixel').Pixel;
+let fs = require('fs');
+let lines
+
+const result = fs.readFileSync('colors.csv', 'utf8');
+lines = result.toString().split("\n");
+getData();
+function getData() {
+    let dict = {}
+    for (const [k, line] of Object.entries(lines)) {
+         let r = line.slice(0,line.length- 1);
+        let values = r.split(',');
+        let key = values[3];
+        if(values[4] == null)
+            break;
+        let RGB = values[4].split(';');
+        let p = new Pixel(RGB[0], RGB[1], RGB[2]);
+        dict[key] = p;
+      }
+    console.log(dict)
+    return dict;
+}
