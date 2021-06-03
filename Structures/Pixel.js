a3d516bb98aa8d08a45ae786f6b11e583cbec037
Yair Yardeni <yair.yardeni@live.biu.ac.il>
2021-06-03 16:23:25 +0300

    added Color profile and Pixels

---
+++ Structures/Pixel.js
@@ -0,0 +1,8 @@
+class Pixel{
+    constructor(red, green, blue) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+}
+module.exports = {Pixel};
---
+++ Structures/colorProfile.js
@@ -0,0 +1,8 @@
+// const Pixel = require('./Pixel').Pixel;
+class colorProfile{
+    constructor(pixel) {
+        this.pixel = pixel;
+        this.count = 0;
+    }
+}
+module.exports = {colorProfile};
